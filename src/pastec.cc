b5e3d724a1944d8e38f5986316d8bf82b6c499df
Ivan Jaramillo <ivanchojara@gmail.com>
2016-03-18 23:32:39 -0500

    support for tags

--- src/pastec.cc
+++ src/pastec.cc
@@ -53,12 +53,12 @@ NAN_METHOD(Pastec::addImage) {
 
   Pastec* pastec = Nan::ObjectWrap::Unwrap<Pastec>(info.This());
 
-  if (info.Length() < 2) {
+  if (info.Length() < 3) {
     Nan::ThrowError("Wrong number of arguments");
     return;
   }
 
-  if (!info[0]->IsNumber() || !info[1]->IsObject()) {
+  if (!info[0]->IsNumber() || !info[1]->IsObject() || !info[2]->IsString()) {
     Nan::ThrowError("Wrong arguments");
     return;
   }
@@ -80,6 +80,14 @@ NAN_METHOD(Pastec::addImage) {
   Nan::Set(resObj, Nan::New("image_id").ToLocalChecked(), Nan::New(i_imageId));
   Nan::Set(resObj, Nan::New("features_extracted").ToLocalChecked(), Nan::New(i_nbFeaturesExtracted));
 
+  if(i_ret == IMAGE_ADDED){
+      string dataStr = string(*Nan::Utf8String(info[2]));
+      u_int32_t t_ret = pastec->index->addTag(i_imageId, dataStr);
+
+      if(t_ret == IMAGE_TAG_ADDED)
+          Nan::Set(resObj, Nan::New("tag").ToLocalChecked(), Nan::New(dataStr).ToLocalChecked());
+  }
+
   info.GetReturnValue().Set(resObj);
 
 }
@@ -93,27 +101,37 @@ NAN_METHOD(Pastec::searchImage) {
     return;
   }
 
-  char *p_imgData = (char *) node::Buffer::Data(info[0]->ToObject());
-  unsigned i_imgSize = node::Buffer::Length(info[0]->ToObject());
+  Local<v8::Object> bufObj = info[0]->ToObject();
 
-  SearchRequest req;
+  char *p_imgData = (char *) node::Buffer::Data(bufObj);
+  unsigned i_imgSize = node::Buffer::Length(bufObj);
 
   vector<char> imgData(i_imgSize);
   memcpy(imgData.data(), p_imgData, i_imgSize);
 
+  SearchRequest req;
   req.imageData = imgData;
   req.client = NULL;
+
   u_int32_t i_ret = pastec->imageSearcher->searchImage(req);
 
   // Create a new empty array.
   Local<v8::Array> results = Nan::New<v8::Array>(req.results.size());
 
-  for (unsigned i = 0; i < req.results.size(); ++i)
-      Nan::Set(results, i, Nan::New(req.results[i]));
+  for (unsigned i = 0; i < req.results.size(); ++i){
+
+      Local<v8::Object> resObj = Nan::New<v8::Object>();
+      Nan::Set(resObj, Nan::New("image_id").ToLocalChecked(), Nan::New(req.results[i]));
+      Nan::Set(resObj, Nan::New("score").ToLocalChecked(), Nan::New(req.scores[i]));
+      Nan::Set(resObj, Nan::New("tag").ToLocalChecked(), Nan::New(req.tags[i]).ToLocalChecked());
+
+      Nan::Set(results, i, resObj);
+
+  }
 
   Local<v8::Object> resObj = Nan::New<v8::Object>();
   Nan::Set(resObj, Nan::New("type").ToLocalChecked(), Nan::New(Converter::codeToString(i_ret).c_str()).ToLocalChecked());
-  Nan::Set(resObj, Nan::New("image_ids").ToLocalChecked(), results);
+  Nan::Set(resObj, Nan::New("results").ToLocalChecked(), results);
 
   info.GetReturnValue().Set(resObj);
 
